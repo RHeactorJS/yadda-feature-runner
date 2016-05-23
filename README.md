f5d2460baad2ae7672bc7be63f359b769870cbe0
Markus Tacker <m@cto.hiv>
2016-05-23 11:39:35 +0200

    feat(Release): extracted from netwoRHk

---
+++ README.md
@@ -0,0 +1,10 @@
+# rheactor-yadda-feature-runner
+
+[![Build Status](https://travis-ci.org/RHeactor/yadda-feature-runner.svg?branch=master)](https://travis-ci.org/RHeactor/yadda-feature-runner)
+[![monitored by greenkeeper.io](https://img.shields.io/badge/greenkeeper.io-monitored-brightgreen.svg)](http://greenkeeper.io/) 
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![semantic-release](https://img.shields.io/badge/semver-semantic%20release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+
+[![NPM](https://nodei.co/npm/rheactor-yadda-feature-runner.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/rheactor-yadda-feature-runner/)
+
+A feature runner for [yadda](https://github.com/acuminous/yadda)
---
+++ index.js
@@ -0,0 +1,78 @@
+'use strict'
+
+/* global featureFile, scenarios, steps, before */
+
+const Yadda = require('yadda')
+const toposort = require('toposort')
+const _filter = require('lodash/filter')
+const _map = require('lodash/map')
+Yadda.plugins.mocha.StepLevelPlugin.init()
+
+function importLibrary (stepdefs, stepdef) {
+  let fileName = stepdef.replace('.js', '')
+  let library = require(fileName)
+  return stepdefs.concat(library)
+}
+
+function run (app, featureFiles, featureLibraries) {
+  // Sort the features by @After annotation
+  let featureGraph = _map(featureFiles, (f) => {
+    let featureNameAndFile
+    featureFile(f, (feature) => {
+      let name = f.match(/\/([^\/\.]+)\.feature$/)
+      featureNameAndFile = [feature.annotations.after, name[1]]
+    })
+    return featureNameAndFile
+  })
+
+  let sortedFeatures = _filter(toposort(featureGraph), (feature) => {
+    return feature !== undefined
+  })
+
+  // Init Yadda
+  let context = {
+    $app: app
+  }
+  let stepDefs = featureLibraries.reduce(importLibrary, [])
+  let y = new Yadda.Yadda(_map(stepDefs, (def) => {
+    return def.library
+  }), {
+    ctx: context
+  })
+  let beforeScenarioHooks = _filter(_map(stepDefs, (def) => {
+    return def.beforeScenario
+  }), (hook) => {
+    return hook !== undefined
+  })
+  _map(sortedFeatures, (featureName) => {
+    let file = _filter(featureFiles, (file) => {
+      return new RegExp('/' + featureName + '.feature$').test(file)
+    })[0]
+    featureFile(file, (feature) => {
+      if (feature.annotations.pending) {
+        console.log('Skipped pending feature: ' + feature.title)
+        return
+      }
+      scenarios(feature.scenarios, (scenario) => {
+        before(() => {
+          _map(beforeScenarioHooks, (hook) => {
+            hook(context)
+          })
+        })
+        if (scenario.annotations.pending) {
+          console.log('Skipped pending scenario: ' + scenario.title)
+          return
+        }
+        steps(scenario.steps, (step, done) => {
+          y.run(step, done)
+        })
+      })
+    })
+  })
+}
+
+module.exports = (app) => {
+  return {
+    run: run.bind(null, app)
+  }
+}
